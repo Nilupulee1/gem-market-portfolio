20c547ab25cb64c227502ae0d0d6576b76c222c0
Nilupulee1 <geethmanilupuli@gmail.com>
2026-05-11 13:11:28 +0530

    feat: refactor BuyerPage to BuyerDashboard and remove unused pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import SellerDashboard from './components/seller/SellerDashboard';
 import AdminDashboard from './components/admin/AdminDashboard';
-import BuyerPage from './pages/BuyerPage';
+import BuyerDashboard from './components/buyer/BuyerDashboard';
 import { UserRole } from './types';
 import {
   ArrowRight,
@@ -498,7 +498,7 @@ const AppLayout = () => {
           path="/buyer/*"
           element={
             <ProtectedRoute allowedRoles={[UserRole.BUYER]}>
-              <BuyerPage />
+              <BuyerDashboard />
             </ProtectedRoute>
           }
         />
--- frontend/src/pages/BuyerPage.tsx
+++
@@ -1,7 +0,0 @@
-import BuyerDashboard from '../components/buyer/BuyerDashboard';
-
-const BuyerPage = () => {
-  return <BuyerDashboard />;
-};
-
-export default BuyerPage;
